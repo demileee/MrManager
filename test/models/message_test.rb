83aa0d8e484b6e39719b14833adc9ec2d1a62d23
Abby Christens <achristens@gmail.com>
2017-11-21 20:34:15 -0500

    user can create message test

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "user can create message" do
+    user = users(:dave)
+    message = Message.new(message_body: "Did you call the shelter?")
+    message.user_id = user.id
+    assert_equal 1, message.user_id
+  end
+
 end
