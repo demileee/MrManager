38f7e7f3d2aa7cb08759ca95ad062d0579bcd0be
Abby Christens <achristens@gmail.com>
2017-11-21 20:55:22 -0500

    test message cannot save without message_body

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-
   test "user can create message" do
     user = users(:dave)
     message = Message.new(message_body: "Did you call the shelter?")
@@ -14,4 +13,19 @@ class MessageTest < ActiveSupport::TestCase
     message.user_id = nil
     refute message.valid?
   end
+
+  test "message does not save without a body" do
+    message = build(:message)
+    message.message_body = nil
+    refute message.valid?
+  end
+  # test "only project owner can assign announcement" do
+  #   project = build(:project)
+  #   user = project.user
+  #   user_two = build(:user)
+  #   user_two.save
+  #   message = build(:message, user_id: user)
+  #   message.announcement =
+  # end
+  # project, , :message_body, presence: true
 end
