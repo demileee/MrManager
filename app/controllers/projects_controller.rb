3458fea5119d74b398455127222f70b2eba7e41a
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-22 23:38:14 -0500

    Define ensure_ownership before action, for edit update and destroy. Only project owners can perform these actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :load_project, only: %i(show edit update destroy)
+  before_action :ensure_ownership, only: %i(edit update destroy)
 
   def index
     @owned_projects = current_user.projects
@@ -56,5 +57,12 @@ class ProjectsController < ApplicationController
 
   def load_project
     @project = Project.find(params[:id])
+  end
+
+  def ensure_ownership
+    if @project.user != current_user
+      flash[:alert] = "Only the project owner can perform this action."
+      redirect_to projects_url
+    end
   end
 end
