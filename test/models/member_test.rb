af7ef259ff857f4f981386bbf65622203951c525
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-22 19:36:39 -0500

    Members tests, can create member, cannot create member if project or user does not exist

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create project member" do
+    member = build(:member)
+    member.save
+    assert member.persisted?
+  end
+
+  test "cannot create member if project does not exist" do
+    member = build(:member, project: nil)
+    member.project = build(:project)
+    member.save
+    refute member.persisted?
+  end
+
+  test "cannot create member if user does not exist" do
+    member = build(:member, user: nil)
+    member.user = build(:user)
+    member.save
+    refute member.persisted?
+  end
+
+# 2. cannot create member if project/user does not exist
+# 3. only project owner can assign/edit/delete role to member
+# 4. only project owner can create member
 end
