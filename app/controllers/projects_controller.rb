bc3fdd69b71832afdcf74803f0e513d7deac01d4
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-22 23:21:32 -0500

    Define project#edit and #update. Create project form partial, render in new and edit views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :load_project, only: %i(show edit update)
+
   def index
     @owned_projects = current_user.projects
     @projects = Member.where('user_id = ?', "#{current_user.id}")
@@ -26,9 +28,18 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
+    @project.update(project_params)
+
+    if @project.save
+      flash.now[:notice] = "#{@project.title} has been successfully updated!"
+      redirect_to projects_url
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -38,5 +49,9 @@ class ProjectsController < ApplicationController
 
   def project_params
     params.require(:project).permit(:title, :description, :soft_deadline, :hard_deadline)
+  end
+
+  def load_project
+    @project = Project.find(params[:id])
   end
 end
