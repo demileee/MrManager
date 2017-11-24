8ab6615e3d3f5eee2e5bffe0c7871f408e1e7748
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-24 17:47:12 -0500

    Write Controller tests for user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "can see home page" do
     get '/'
-    assert response.status == 200
+    assert_response :success
+    assert_select 'a', "Sign up here!"
   end
+
+
 end
