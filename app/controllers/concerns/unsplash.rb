4d49c48f786e6bd7b176ba768cd0dcfec674e5a8
Abby Christens <achristens@gmail.com>
2017-11-25 18:50:01 -0500

    Rendering matching name, link, and image in User show page.

--- app/controllers/concerns/unsplash.rb
+++ app/controllers/concerns/unsplash.rb
@@ -7,13 +7,14 @@ class Unsplash
   def initialize
     response = HTTParty.get("https://api.unsplash.com/photos/random/?client_id=#{ENV["UNSPLASH_KEY"]}&query=nature&count=1")
     parsed_response = JSON.parse(response.body)
-    @portfolio_link = parsed_response[0]["portfolio"]
+    @portfolio_link = parsed_response[0]["user"]["links"]["html"]
     @photographer = parsed_response[0]["user"]["name"]
     @image  = parsed_response[0]["urls"]["full"]
-    puts "#{image}, by #{photographer}"
   end
 
 end
+
+
 
 
 # def self.get_background
