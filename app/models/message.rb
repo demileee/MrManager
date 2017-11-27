0f22d81ad746690ea540a44148cc553ba1a6f6bf
Demi Lee <demilee.to@gmail.com>
2017-11-27 16:30:34 -0500

    implemented user notifications

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,8 @@
 class Message < ApplicationRecord
-  after_save { |message| create_event(message) if message.announcement }
+  after_save do |message|
+    create_event(message) if message.announcement
+    create_notification(message) if message.announcement
+  end
 
   belongs_to :project
   belongs_to :user
@@ -10,5 +13,9 @@ class Message < ApplicationRecord
 
   def create_event(message)
     Event.create message: "A new announcement has been created: #{message.message_body}", project_id: message.project_id
+  end
+
+  def create_notification(message)
+    Notification.create message: "#{message.project.title}: #{message.message_body}", user_id: message.user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171127203927) do
+ActiveRecord::Schema.define(version: 20171127210155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +59,13 @@ ActiveRecord::Schema.define(version: 20171127203927) do
     t.index ["project_id"], name: "index_messages_on_project_id"
     t.index ["task_id"], name: "index_messages_on_task_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
