527124e26b141f48743d332fbdb550b464cf0f55
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-28 17:33:06 -0500

    Bundle and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171128201822) do
 
   # These are extensions that must be enabled in order to support this database
@@ -105,9 +104,9 @@ ActiveRecord::Schema.define(version: 20171128201822) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "task_id"
-    t.datetime "last_read"
     t.string "token"
     t.string "slack_id"
+    t.datetime "last_read"
     t.index ["task_id"], name: "index_users_on_task_id"
   end
 
