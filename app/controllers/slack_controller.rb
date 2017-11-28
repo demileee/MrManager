ad8678bf1a3e7c15d7fbac004a6514db2d2a3976
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-28 15:27:03 -0500

    Save all slack credentials in controller

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,14 +1,24 @@
+require 'securerandom'
+
 class SlackController < ApplicationController
 
   def create
+    pass = SecureRandom.base64
     @user = User.new
     @user.first_name = request.env["omniauth.auth"]["info"]["first_name"]
     @user.last_name = request.env["omniauth.auth"]["info"]["last_name"]
     @user.email = request.env["omniauth.auth"]["info"]["email"]
     @user.token = request.env["omniauth.auth"]["credentials"]["token"]
     @user.slack_id = request.env["omniauth.auth"]["info"]["user_id"]
-    puts params
-    byebug
+    @user.password = pass
+    @user.password_confirmation = pass
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_url(@user)
+    else
+      redirect_to new_user_url
+    end
   end
 
 end
