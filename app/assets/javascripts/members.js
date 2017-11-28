721df9cec39175b4ac45ee303790d570048a0f79
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-28 09:22:57 -0500

    Change id name of datalist and save as vasiable in members.js

--- app/assets/javascripts/members.js
+++ app/assets/javascripts/members.js
@@ -1,2 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function() {
+  var datalist = document.getElementById('ajax-email')
+}
