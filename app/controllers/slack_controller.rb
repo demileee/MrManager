30337ea0cc995674240ef430b93f9da9d6882029
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-28 15:21:25 -0500

    Create migration to save slack info and save detials in controller

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,7 +1,14 @@
 class SlackController < ApplicationController
 
   def create
+    @user = User.new
+    @user.first_name = request.env["omniauth.auth"]["info"]["first_name"]
+    @user.last_name = request.env["omniauth.auth"]["info"]["last_name"]
+    @user.email = request.env["omniauth.auth"]["info"]["email"]
+    @user.token = request.env["omniauth.auth"]["credentials"]["token"]
+    @user.slack_id = request.env["omniauth.auth"]["info"]["user_id"]
     puts params
+    byebug
   end
 
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slack, ENV["MR_MANAGER_ID"], ENV["MR_MANAGER_SECRET"], scope: 'identity.basic', name: :sign_in_with_slack
+  provider :slack, ENV["MRMANAGER_ID"], ENV["MRMANAGER_SECRET"], scope: 'identity.basic'
 end
