99605222e18c48d5f021daf7286dbae28bfca6c8
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-28 19:52:32 -0500

    Define member#invite to send invitation email. Rename mailer views and mailer method to new_member_email

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -25,7 +25,6 @@ class MembersController < ApplicationController
 
     if @member.save
       redirect_to project_url(@project)
-      UserMailer.welcome_email(@member.user).deliver_now
     else
       flash[:alert] = "That user doesn't exist"
       render :new
@@ -51,6 +50,13 @@ class MembersController < ApplicationController
       flash[:alert] = "Did not successfully join"
       render request.referer
     end
+  end
+
+  def invite
+    @new_member_email = params[:email][0]
+    UserMailer.new_member_email(@new_member_email).deliver_now
+    flash[:notice] = "An invitation email has been sent to #{@new_member_email}."
+    redirect_to new_project_member_url
   end
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'mrmanager247@gmail.com'
 
-  def welcome_email(user)
-    @user = user
+  def new_member_email(user_email)
+    @user_email = user_email
     @url = 'http://localhost:3000/'
-    mail(to: @user.email, subject: 'Weclome to Mr Manager!')
+    mail(to: @user_email, subject: 'Welcome to Mr Manager!')
   end
 end
