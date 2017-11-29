23c3c8e46b9c2d549696f3a8235ca8631ae720ae
Abby Christens <achristens@gmail.com>
2017-11-29 16:37:04 -0500

    Resize notification window

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   function toggleDiv(div){
-    if (div.style.display = "none"){
+    if (div.style.display === "none"){
       div.style.display = "block";
     } else {
       div.style.display = "none";
