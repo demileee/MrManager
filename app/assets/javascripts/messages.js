1d13be538b0b68d436f13a735ab092c16147a4a0
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-29 22:03:41 -0500

    Change discussion animations to slide in and out

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -4,26 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
   var discussion = document.getElementById('discussion')
   var messageBoard = document.querySelector('.message-board')
 
-  discussion.classList.add('animated');
-  discussion.style.display = 'none';
-
-  function toggleDiscussion(div){
-    if (div.style.display === 'none'){
-      div.classList.remove('zoomOut')
-      div.classList.add('zoomIn')
-      div.style.display = "block";
-    } else {
-      div.classList.remove('zoomIn')
-      div.classList.add('zoomOut')
-      setTimeout(function() {div.style.display = 'none'}, 200)
-    }
-  }
+  messageBoard.classList.add('animated');
+  messageBoard.style.display = 'none';
 
   discussion.addEventListener('click', function(e) {
     e.preventDefault();
-    toggleDiscussion(discussion)
-  })
-
-
-
+    toggleDivRight(messageBoard);
+  });
 })
