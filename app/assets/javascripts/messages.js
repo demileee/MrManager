755b57fbbdcfdb3a3c2eb2b40d943fe6d7a064f4
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-29 20:52:04 -0500

    Add animation to discussion

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -4,9 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
   var discussion = document.getElementById('discussion')
   var messageBoard = document.querySelector('.message-board')
 
+  discussion.classList.add('animated');
+  discussion.style.display = 'none';
+
+  function toggleDiscussion(div){
+    if (div.style.display === 'none'){
+      div.classList.remove('zoomOut')
+      div.classList.add('zoomIn')
+      div.style.display = "block";
+    } else {
+      div.classList.remove('zoomIn')
+      div.classList.add('zoomOut')
+      setTimeout(function() {div.style.display = 'none'}, 200)
+    }
+  }
+
   discussion.addEventListener('click', function(e) {
     e.preventDefault();
-
+    toggleDiscussion(discussion)
   })
 
 
