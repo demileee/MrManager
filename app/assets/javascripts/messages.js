f80806d1a43428a2bfc09957d3a82944dfd78cbc
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-30 12:44:19 -0500

    Add Material Icons

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,12 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 document.addEventListener('DOMContentLoaded', function() {
-  var discussion = document.getElementById('discussion')
-  var messageBoard = document.querySelector('.message-board')
+  var discussion = document.getElementById('discussion');
+  var messageBoard = document.querySelector('.message-board');
 
   messageBoard.classList.add('animated');
   messageBoard.style.display = 'none';
-  discussion.scrollTop = discussion.scrollHeight;
 
   function toggleMessages(div){
     if (div.style.display === 'none'){
