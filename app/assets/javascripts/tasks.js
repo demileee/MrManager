4e7b3c60de3dd587e196c0c001a6b80fd51b29dd
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-05 12:52:48 -0500

    Remove ajax request from links

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,13 +3,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var completePinnedTask = document.getElementById('completed');
-  var completeTasks = document.querySelectorAll('.complete_task')
+  var completeTasks = document.querySelectorAll('.complete_task');
+  var completeLinks = document.querySelectorAll('.complete_link');
   var checkBox = document.createElement('i');
 
   checkBox.classList.add('material-icons');
   checkBox.innerText = 'check_box_outline_blank'
   completePinnedTask.appendChild(checkBox);
-
 
   completeTasks.forEach(function(task) {
     task.lastChild.innerText = 'check_box_outline_blank';
@@ -18,7 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     task.addEventListener('mouseout', function() {
       task.lastChild.innerText = 'check_box_outline_blank';
-    })
-  })
-
+    });
+  });
 });
