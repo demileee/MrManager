b551974781fa98e4a878363a18e9d72123cec565
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-05 11:39:44 -0500

    Add material icon check box to completed link on pinned task

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function() {
+
+var completePinnedTask = document.getElementById('completed');
+
+var checkBox = document.createElement('i');
+checkBox.classList.add('material-icons');
+checkBox.innerText = 'check_box_outline_blank'
+completePinnedTask.appendChild(checkBox);
+
+
+
+});
