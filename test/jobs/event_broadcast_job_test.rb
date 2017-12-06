90fe916cb55405258b0f6d7248ccc42595102fb7
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-06 18:28:00 -0500

    Test for event of updating a task

--- test/jobs/event_broadcast_job_test.rb
+++ test/jobs/event_broadcast_job_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class EventBroadcastJobTest < ActiveJob::TestCase
-  test "Event is created after joining a project" do
+  test "Event is created after joining a project and making announcement" do
     project = build(:project)
     project.save
     events = Event.where("project_id = ?", project.id)
@@ -9,5 +9,16 @@ class EventBroadcastJobTest < ActiveJob::TestCase
     message = build(:message, user: project.user, project:project, announcement: true)
     message.save
     assert_equal "Announcement: Hey I called the shelter", events.last.message
+  end
+
+  test "Event is broadcast on the updating of a task" do
+    project = build(:project)
+    project.save
+    task = build(:task, user: project.user, project: project)
+    task.save
+    task.task_body = "Do this other thing"
+    task.save
+    events = Event.where("project_id = ?", project.id)
+    assert_equal "Updated task: Do this other thing", events.last.message
   end
 end
