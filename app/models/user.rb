d385f054c2e53d4be32c019223368ca3eb82995c
Demi Lee <demilee.to@gmail.com>
2017-12-06 01:55:45 -0500

    full name capitalized

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   end
 
   def full_name
-    "#{self.first_name} #{self.last_name}"
+    "#{self.first_name.capitalize} #{self.last_name.capitalize}"
   end
 
   def remove_pinned_task
