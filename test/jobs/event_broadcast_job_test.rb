99f594905dcd136692cc8e50f1867158e58e62ec
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-06 18:22:06 -0500

    Test for event being made after an anouncement from Mr Manager

--- test/jobs/event_broadcast_job_test.rb
+++ test/jobs/event_broadcast_job_test.rb
@@ -4,7 +4,10 @@ class EventBroadcastJobTest < ActiveJob::TestCase
   test "Event is created after joining a project" do
     project = build(:project)
     project.save
-    event = Event.where("project_id = ?", project.id)
-    assert_equal "Sally Lee has joined the project.", event.first.message
+    events = Event.where("project_id = ?", project.id)
+    assert_equal "Sally Lee has joined the project.", events.first.message
+    message = build(:message, user: project.user, project:project, announcement: true)
+    message.save
+    assert_equal "Announcement: Hey I called the shelter", events.last.message
   end
 end
