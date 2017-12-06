96903c5ac39998006da97849017ea6cd816e4547
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-06 17:46:43 -0500

    Create tests for project tasks completed

--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class MembersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "percentage of a members completed tasks with no tasks" do
+    member = build(:member)
+    assert_equal "", member.project_tasks_completed_percent(member.project)
+  end
+
+  test "percentage of a members completed tasks" do
+    member = build(:member)
+    member.save
+    task = build(:task, project: member.project, user: member.user)
+    task.completed
+    task.save
+    assert_equal "50", member.project_tasks_completed_percent(member.project)
+  end
 end
