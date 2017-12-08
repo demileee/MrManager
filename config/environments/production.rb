5e027a63522048f6827915c3b817bb072db0fe53
Demi Lee <demilee.to@gmail.com>
2017-12-08 17:05:45 -0500

    https protocol in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
+  Rails.application.routes.default_url_options[:protocol] = 'https'
   config.active_job.queue_adapter = :delayed_job
   # Code is not reloaded between requests.
   config.cache_classes = true
