ff35519cfd1604d8064a40fe4e51ca042aaeb0b6
Abby Christens <achristens@gmail.com>
2017-12-08 12:04:24 -0500

    Styling link divs

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -2,11 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
   var body = document.querySelector('body');
   var whiteLogo = document.querySelector('.logo-white');
   var blackLogo = document.querySelector('.logo-black');
-
+  var loginButton = document.querySelector('.login');
+  var signupButton = document.querySelector('.sign-up');
 
   if (body.style.color === 'white'){
     whiteLogo.style.display = 'block';
   } else {
     blackLogo.style.display = 'block';
+    loginButton.style.borderColor = 'black';
+    signupButton.style.borderColor = 'black';
   }
 });
